c862b06e2a6baa5bbb6ac16aef96f84dc301f3ca
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2024-11-27 23:16:39 +1000

    Add files via upload

---
+++ WpfApp8/WpfApp3/Window1.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private List<string> ImageGallery;
+
+
+
+        private void UpdateImageDisplay()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\1";
+            if (Directory.Exists(path))
+            {
+                ImageGallery = new List<string>(Directory.GetFiles(path, "*.jpg"));
+                ImageGallery.AddRange(Directory.GetFiles(path, "*.png"));
+                ImageGallery.AddRange(Directory.GetFiles(path, "*.jpeg"));
+
+                int index = 0;
+                if (RadioImage1.IsChecked == true)
+                {
+                    index = 0;
+                }
+                else if (RadioImage2.IsChecked == true)
+                {
+                    index = 1;
+                }
+                else if (RadioImage3.IsChecked == true)
+                {
+                    index = 2;
+                }
+
+                if (ImageGallery.Count < index+1)
+                {
+                    MessageBox.Show("Индекс выходит за границы листа");
+                    return;
+                }
+
+                RenderImageLabel.Source = new BitmapImage(new Uri(ImageGallery[index]));
+            }
+            
+
+        }   
+
+        public Window1()
+        {
+            InitializeComponent();
+            UpdateImageDisplay();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow next = new MainWindow();
+            next.Show();
+            this.Close();
+        }
+
+        private void RadioImage1_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageDisplay();
+        }
+
+        private void RadioImage2_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageDisplay();
+        }
+
+        private void RadioImage3_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageDisplay();
+        }
+    }
+}
---
+++ WpfApp8/WpfApp3/Window2.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Image|*.png;*.jpg;*.jpeg";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+
+                
+                ProgressBar.Visibility = Visibility.Visible;
+                ProgressBar.Value = 0;
+
+                await Task.Run(() =>
+                {
+                    
+                    for (int i = 0; i <= 100; i++)
+                    {
+                        Dispatcher.Invoke(() => ProgressBar.Value = i); 
+                        System.Threading.Thread.Sleep(10); 
+                    }
+                });
+
+                
+                ShowerImage.Source = new BitmapImage(new Uri(filename));
+
+                
+                ProgressBar.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+            w.Show();
+            this.Close();
+        }
+    }
+}
+
